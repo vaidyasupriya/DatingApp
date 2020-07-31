6e92beb3a6c22fa9902cbfc815f25526897f2612
Supriya Vaidya <vaidya.supriya@gmail.com>
2020-07-31 16:29:50 +0200

    Pre-publishing

--- DatingApp.API/Migrations/20190604115711_InitialCreate.cs
+++ DatingApp.API/Migrations/20190604115711_InitialCreate.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace DatingApp.API.Migrations
 {
@@ -11,7 +12,9 @@ namespace DatingApp.API.Migrations
                 columns: table => new
                 {
                     Id = table.Column<int>(nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
+                    .Annotation("SqlServer:ValueGenerationStrategy",
+                    SqlServerValueGenerationStrategy.IdentityColumn)                    
+                    .Annotation("Sqlite:Autoincrement", true),
                     Name = table.Column<string>(nullable: true)
                 },
                 constraints: table =>
--- DatingApp.API/Migrations/20190927073203_ExtendedUserClass.cs
+++ DatingApp.API/Migrations/20190927073203_ExtendedUserClass.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace DatingApp.API.Migrations
@@ -65,6 +66,8 @@ namespace DatingApp.API.Migrations
                 columns: table => new
                 {
                     Id = table.Column<int>(nullable: false)
+                    .Annotation("SqlServer:ValueGenerationStrategy",
+                    SqlServerValueGenerationStrategy.IdentityColumn)    
                         .Annotation("Sqlite:Autoincrement", true),
                     Url = table.Column<string>(nullable: true),
                     DateAdded = table.Column<DateTime>(nullable: false),
